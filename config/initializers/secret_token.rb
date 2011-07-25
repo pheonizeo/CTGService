c52b7f4ba86b34d3285d252ea80ff8ccf13b2115
pheonize <pheonizeo@gmail.com>
2011-07-26 01:45:24 +0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CTGService::Application.config.secret_token = 'ff63f03f5c58d0ded0d2427b32069583a6674235b3f7a108c501a5614ea006506b5b9464f88e09dbdfc5341cdc42fb03aafcc7beb234876b38c6e82974e181ec'

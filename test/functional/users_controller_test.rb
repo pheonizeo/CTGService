010f8a97bcf003791bc4a1c48a1f1b2ab023461e
pheonize <pheonizeo@gmail.com>
2011-07-25 23:41:26 +0700

    user helper fixed

--- test/functional/users_controller_test.rb
+++ test/functional/users_controller_test.rb
@@ -2,48 +2,25 @@ require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
   setup do
-    @user = users(:one)
+@input_attributes = {
+  :name => "admin",
+  :password => "private",
+  :password_confirmation => "private"
+}
+  @user = users(:one)
   end
 
-  test "should get index" do
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:users)
-  end
-
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
-
+  #...
   test "should create user" do
     assert_difference('User.count') do
-      post :create, :user => @user.attributes
-    end
+       post :create, :user => @input_attributes
 
-    assert_redirected_to user_path(assigns(:user))
   end
-
-  test "should show user" do
-    get :show, :id => @user.to_param
-    assert_response :success
+    assert_redirected_to users_path
   end
-
-  test "should get edit" do
-    get :edit, :id => @user.to_param
-    assert_response :success
-  end
-
+  #...
   test "should update user" do
-    put :update, :id => @user.to_param, :user => @user.attributes
-    assert_redirected_to user_path(assigns(:user))
-  end
-
-  test "should destroy user" do
-    assert_difference('User.count', -1) do
-      delete :destroy, :id => @user.to_param
-    end
-
+    put :update, :id => @user.to_param, :user => @input_attributes
     assert_redirected_to users_path
   end
 end

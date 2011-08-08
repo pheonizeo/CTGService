36fc69680f8e2ee43267c7186333ae4d302769ae
pheonize <pheonizeo@gmail.com>
2011-08-08 22:09:24 +0700

    sign up error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,14 @@
 class UsersController < ApplicationController
 
+
   # GET /users
   # GET /users.xml
   def index
-    @users = User.all
+    @users = User.order(:name)
 
     respond_to do |format|
       format.html # index.html.erb
-      format.xml  { render :xml => @users.to_xml }
+      format.xml { render :xml => @users }
     end
   end
 
@@ -16,9 +17,10 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
 
+
     respond_to do |format|
       format.html # show.html.erb
-      format.xml  { render :xml => @user.to_xml }
+      format.xml { render :xml => @user }
     end
   end
 
@@ -29,9 +31,9 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       format.html # new.html.erb
-      format.xml  { render :xml => @user.to_xml}
+      format.xml { render :xml => @user }
     end
-    redirect_to (:controller=>'events' ,:action =>'index')
+
   end
 
   # GET /users/1/edit
@@ -47,13 +49,13 @@ class UsersController < ApplicationController
     respond_to do |format|
       if @user.save
         format.html { redirect_to(users_url,
-            :notice => "User #{@user.name} was successfully created.") }
-        format.xml  { render :xml => @user,
-            :status => :created, :location => @user }
+                                  :notice => "User #{@user.name} was successfully created.") }
+        format.xml { render :xml => @user,
+                            :status => :created, :location => @user }
       else
         format.html { render :action => "new" }
-        format.xml  { render :xml => @user.errors,
-            :status => :unprocessable_entity }
+        format.xml { render :xml => @user.errors,
+                            :status => :unprocessable_entity }
       end
     end
   end
@@ -66,12 +68,12 @@ class UsersController < ApplicationController
     respond_to do |format|
       if @user.update_attributes(params[:user])
         format.html { redirect_to(users_url,
-            :notice => "User #{@user.name} was successfully updated.") }
-        format.xml  { head :ok }
+                                  :notice => "User #{@user.name} was successfully updated.") }
+        format.xml { head :ok }
       else
         format.html { render :action => "edit" }
-        format.xml  { render :xml => @user.errors,
-            :status => :unprocessable_entity }
+        format.xml { render :xml => @user.errors,
+                            :status => :unprocessable_entity }
       end
     end
   end
@@ -80,11 +82,17 @@ class UsersController < ApplicationController
   # DELETE /users/1.xml
   def destroy
     @user = User.find(params[:id])
-    @user.destroy
+    begin
+      flash[:notice] = "User #{@user.name} delete"
+      @user.destroy
+    rescue Exeption => e
+      flash[:notice] = e.message
+    end
 
     respond_to do |format|
       format.html { redirect_to(users_url) }
-      format.xml  { head :ok }
+      format.xml { head :ok }
     end
   end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,47 +1,54 @@
 require 'digest/sha2'
-class User < ActiveRecord::Base
-  validates :name ,:presence => true, :uniqueness =>true
 
-  validates :password, :confirmation =>true
+class User < ActiveRecord::Base
+
+  validates_presence_of :name
+  validates_uniqueness_of :name
+
   attr_accessor :password_confirmation
-  attr_reader :password
+  validates_confirmation_of       :password
 
   validate :password_must_be_present
 
-class << self
-  def authenticate(name, password)
-    if user = find_by_name(name)
-      if user.hashed_password == encrypt_password(password, user.salt)
-        user
-      end
+ # class << self
+    def self.authenticate(name, password)
+       user = self.find_by_name(name)
+        if user
+          expected_password = encrypted_password(password,user.salt)
+          if user.hashed_password != expected_password
+            user =nil
+          end
+        end
+      user
     end
-end
 
-  def encrypt_password(password, salt)
-    Digest::SHA2.hexdigest(password + "wibble" + salt)
-  end
-end
+    def encrypt_password(password, salt)
+      Digest::SHA2.hexdigest(password + "wibble" + salt)
+    end
+  #end
 
 # 'password' is a virtual attribute
-def password=(password)
-  @password = password
-
-  if password.present?
-    generate_salt
-    self.hashed_password = self.class.encrypt_password(password, salt)
-end end
+  def password=(password)
+    @password = password
 
 
-
+    if password.present?
+      generate_salt
+      self.hashed_password = self.class.encrypt_password(password, salt)
+    end
+  end
+         def password
+           @password
+         end
 
   private
-    def password_must_be_present
-      errors.add(:password, "Missing password") unless hashed_password.present?
-    end
+  def password_must_be_present
+    errors.add(:password, "Missing password") unless hashed_password.present?
+  end
 
-    def  generate_salt
-      self.salt= self.object_id.to_s
+  def generate_salt
+    self.salt= self.object_id.to_s
 
-    end
+  end
 end
 

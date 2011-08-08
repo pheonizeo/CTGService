bc20a172f3000fe3225afc43531bcbeaba52a0b1
pheonize <pheonizeo@gmail.com>
2011-08-08 23:02:17 +0700

    fix bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,54 +1,63 @@
-require 'digest/sha2'
+require 'digest/sha1'
+
 
 class User < ActiveRecord::Base
-
-  validates_presence_of :name
-  validates_uniqueness_of :name
-
+  
+  validates_presence_of     :name
+  validates_uniqueness_of   :name
+ 
   attr_accessor :password_confirmation
-  validates_confirmation_of       :password
+  validates_confirmation_of :password
 
-  validate :password_must_be_present
+  validate :password_non_blank
+  
 
- # class << self
-    def self.authenticate(name, password)
-       user = self.find_by_name(name)
-        if user
-          expected_password = encrypted_password(password,user.salt)
-          if user.hashed_password != expected_password
-            user =nil
-          end
-        end
-      user
+  
+  def self.authenticate(name, password)
+    user = self.find_by_name(name)
+    if user
+      expected_password = encrypted_password(password, user.salt)
+      if user.hashed_password != expected_password
+        user = nil
+      end
     end
-
-    def encrypt_password(password, salt)
-      Digest::SHA2.hexdigest(password + "wibble" + salt)
-    end
-  #end
-
-# 'password' is a virtual attribute
-  def password=(password)
-    @password = password
-
-
-    if password.present?
-      generate_salt
-      self.hashed_password = self.class.encrypt_password(password, salt)
-    end
+    user
   end
-         def password
-           @password
-         end
+  
+  
+  # 'password' is a virtual attribute
+  
+  def password
+    @password
+  end
+  
+  def password=(pwd)
+    @password = pwd
+    return if pwd.blank?
+    create_new_salt
+    self.hashed_password = User.encrypted_password(self.password, self.salt)
+  end
+  
+  
 
-  private
-  def password_must_be_present
-    errors.add(:password, "Missing password") unless hashed_password.present?
+private
+
+  def password_non_blank
+    errors.add(:password, "Missing password") if hashed_password.blank?
   end
 
-  def generate_salt
-    self.salt= self.object_id.to_s
-
+  
+  
+  def create_new_salt
+    self.salt = self.object_id.to_s + rand.to_s
   end
+  
+  
+  
+  def self.encrypted_password(password, salt)
+    string_to_hash = password + "wibble" + salt
+    Digest::SHA1.hexdigest(string_to_hash)
+  end
+  
+
 end
-

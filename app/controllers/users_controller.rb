7c0b09a059d81e3e1789355b6686af33f4c62c6c
runganunt toankhamsont <pheonizeo@gmail.com>
2011-09-04 15:01:21 +0700

    protect login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 
-  before_filter :authenticate, :except => [:login]
+  #before_filter :authenticate, :except => [:login]
   # GET /users
   # GET /users.xml
   def index
@@ -100,9 +100,9 @@ class UsersController < ApplicationController
 
   end
 
-  protected
-  def authenticate
+  #protected
+  #def authenticate
 
-  end
+  #end
 
 end

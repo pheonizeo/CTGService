e9752be5e5265641ba8593c8213ed224266bd719
runganunt toankhamsont <pheonizeo@gmail.com>
2011-09-06 15:50:40 +0700

    last version

--- app/models/upcomingevent.rb
+++ app/models/upcomingevent.rb
@@ -1,9 +1,10 @@
 class Upcomingevent < ActiveRecord::Base
+
   validates :name, :presence =>true
   validates :location, :presence =>true
   validates :description, :presence =>true
 
-  validates :timestart, :presence => true
+  validates :timestart, d :presence => true
   validates :timeexpire, :presence => true
 
 
